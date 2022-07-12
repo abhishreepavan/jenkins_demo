9544440cd989bb0eff5439400786f5f1074274e7
abhishreepavan <72576507+abhishreepavan@users.noreply.github.com>
2022-07-12 22:21:25 +0530

    Update source.py

--- source.py
+++ source.py
@@ -5,7 +5,7 @@ import git
 from sys import argv
 import subprocess
 
-repo = git.Repo("/home/ec2-user/jenkins_demo")
+repo = git.Repo("/home/ec2-user/ansible/jenkins_demo")
 
 repoDiffs = repo.head.commit.diff('HEAD~1')
 path=[]

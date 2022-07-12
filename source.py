f206be300ef6c1093c592165c524fa2e79698c3e
abhishreepavan <72576507+abhishreepavan@users.noreply.github.com>
2022-07-12 22:22:10 +0530

    Update source.py

--- source.py
+++ source.py
@@ -23,8 +23,8 @@ for item in repoDiffs:
            destination = src_load[service]['dest']
            print(source)
            print(destination)
-           cmd = ["ansible-playbook","-i inventory","ansible-playbook.yml","-extra-vars src=source dest=destination service=service"]
-           subprocess.call(cmd)
+         cmd = ["ansible-playbook","-i inventory","ansible-playbook.yml","-extra-vars src=source dest=destination service=service"]
+         subprocess.call(cmd)
 
     except ValueError:
         print ("input correct service file")
